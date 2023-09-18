70c6bbb7544036448e060509923af646f248d5ed
richard@androidminds.cn <Richard@androiminds.cn>
2023-09-18 18:04:58 +0800

    show full pdf

--- src/components/PDF.vue
+++ src/components/PDF.vue
@@ -1,47 +1,71 @@
 <template>
     <div>
-        <canvas ref="canvas" class="w-full h-full"></canvas>
+        <canvas class="w-full h-full" v-for="index in pageCount" :key="index" :ref="canvasRefs[index - 1]"/>
     </div>
 </template>
   
 <script lang="ts" setup>
-import {onMounted, ref } from 'vue';
+import {onMounted, ref, Ref } from 'vue';
 
 import * as pdfjs from 'pdfjs-dist';
 import PDFJSWorker from "pdfjs-dist/build/pdf.worker?url";
 
 
 const props = defineProps({
-    url: {
-        type: String,
-        required: true,
-    },
+    url: { type: String, required: true,},
 },)
-const canvas = ref<any>(null);
-let ctx: CanvasRenderingContext2D | null = null;
+
+
+let pageCount = ref(0)
+
+const canvasRefs = ref<Array<Ref<Array<HTMLCanvasElement>>>>([]);
+let dpr = 1
 
 onMounted(() => {
+    dpr = window.devicePixelRatio || 1;
     loadPdf()
 });
 
+
 async function loadPdf() {
     try {
-
-        ctx = canvas.value.getContext('2d');
-
         pdfjs.GlobalWorkerOptions.workerSrc = PDFJSWorker
+
         const loadingTask = pdfjs.getDocument(props.url);
+        
         const pdf = await loadingTask.promise;
-        const page = await pdf.getPage(1);
 
-        const viewport = page.getViewport({ scale: 1 });
+        const refs = [];
+        for (let i = 0; i < pdf.numPages; i++) {
+            refs.push(ref() as Ref<Array<HTMLCanvasElement>>);
+        }
 
-        const renderContext: any = {
-            canvasContext: ctx,
-            viewport: viewport,
-        };
+        canvasRefs.value = refs;
+        pageCount.value = pdf.numPages;
 
-        page.render(renderContext);
+        for (let i = 0; i < pdf.numPages; i++) {
+            const page = await pdf.getPage(i + 1);
+
+            const viewport = page.getViewport({ scale: 1 });
+
+            const canvas = canvasRefs.value[i].value[0];
+
+            const ctx = canvas.getContext('2d');
+
+            var scale = (canvas.parentNode as HTMLDivElement).clientWidth / viewport.width;
+
+            const scaledViewport = page.getViewport({ scale: scale * dpr });
+
+            canvas.width = scaledViewport.width;
+            canvas.height = scaledViewport.height;
+
+            const renderContext: any = {
+                canvasContext: ctx,
+                viewport: scaledViewport,
+            };
+
+            page.render(renderContext);
+        }
     } catch (error) {
         console.error(error);
     }
